d67a8f7be1140591568e6f6dd4952059ace25232
安佐 <2485254991@qq.com>
2023-04-10 09:18:51 +0800

    ChatGpt

---
+++ src/test/java/com/example/springbootvueproject/SpringBootVueProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootvueproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVueProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
